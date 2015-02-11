5fe36aee6773e6318a03169fd22799294998935a
Brandon Antonelli <bantonelli07@gmail.com>
2015-02-10 23:14:43 -0500

    Set up uniqueSampleTypes computed property for the 'your kit' section of kitbuilder.

--- app/controllers/your-kit.js
+++ app/controllers/your-kit.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     needs: ['kitbuilder'],
-    customKitName: null
+    customKitName: null,
+    chosenSampleTypes: function () {
+        return this.get('controllers.kitbuilder.samplesChosen').mapBy('_data.type');
+    }.property('controllers.kitbuilder.samplesChosen.@each'),
+    uniqueSampleTypes: Ember.computed.uniq('chosenSampleTypes')
 });
