596dfc4cdfb9ed708a6ed82f15788578f136394e
Brandon Antonelli <bantonelli07@gmail.com>
2015-04-03 03:34:04 -0400

    Implement checkout invoice with styling and basic functionality. Install ember-cli-accounting package to format money easily.

--- app/controllers/kb-checkout.js
+++ app/controllers/kb-checkout.js
@@ -1,11 +1,40 @@
 import Ember from 'ember';
 import config from '.././config/environment';
+import accounting from "accounting";
 var swal = window.sweetAlert;
 
 
 export default Ember.Controller.extend({
     needs: ['kitbuilder', 'your-kit'],  
     purchasedKitID: null,   
+    chosenSampleKits: function () {
+        return this.get('controllers.kitbuilder.samplesChosen').mapBy('kit');
+    }.property('controllers.kitbuilder.samplesChosen.@each'),
+    uniqueSampleKits: Ember.computed.uniq('chosenSampleKits'),
+    kitsChosen: Ember.computed('uniqueSampleKits', 'controllers.kitbuilder.samplesChosen.@each', function (key, value){
+    	var result = [];
+    	var uniqueSampleKits = this.get('uniqueSampleKits');
+    	var samplesChosen = this.get('controllers.kitbuilder.samplesChosen');
+    	for (var kitIndex = 0; kitIndex < uniqueSampleKits.length; kitIndex++){
+    		var kitName = uniqueSampleKits[kitIndex]._data.name;
+    		var kitID = uniqueSampleKits[kitIndex]._data.id;
+    		var kitObject = {name: kitName, numberOfSamples: 0};
+    		var filtered = samplesChosen.filterBy('kit.name', kitName);
+    		kitObject.numberOfSamples += filtered.length;
+    		kitObject.subTotal = kitObject.numberOfSamples * 0.50;
+    		kitObject.subTotalFormatted = accounting.formatMoney(kitObject.numberOfSamples * 0.50);
+    		result.pushObject(kitObject);
+    	}
+    	return result;
+    }),
+    totalPrice: Ember.computed('kitsChosen', function (key, value){
+    	var result = 0;
+    	var kitsChosen = this.get('kitsChosen');
+    	for (var i = 0; i < kitsChosen.length; i++){
+    		result += kitsChosen[i].subTotal;
+    	}
+    	return accounting.formatMoney(result);
+    }),
     actions: {        
     }
 });
