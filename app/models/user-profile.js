f1e625767c9e7fe59f7e1174796436634bd7a532
Brandon Antonelli <bantonelli07@gmail.com>
2015-05-22 23:47:36 -0400

    - Set up api/v1 compatible user-profile model.

--- app/models/user-profile.js
+++ app/models/user-profile.js
@@ -1,9 +1,21 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    user: DS.belongsTo('user'),
+    username: DS.attr('string'),
+    image: DS.attr('string'),
     last_4_digits: DS.attr( 'string' ),
     created_at: DS.attr('date'),
     updated_at: DS.attr('date'),
-    custom_kits: DS.hasMany('custom-kit', {async: true})
+    kitbuilder_purchases: DS.hasMany('kitbuilder-purchase', {async: true}),
+    kitbuilder_templates: DS.hasMany('kitbuilder-template', {async: true}),
+    samples_purchased: DS.attr(),
+    followers: DS.hasMany('follower', {async: true}),
+    templates_followed: DS.hasMany('kitbuilder-template', {async: true})   	
 });
+
+// when accessing the variables template_followed/followers you need to do:
+	// profile.followers.content. 
+	// profile.followers will return a promise object.
+
+// Samples purchased: just a list object of ID's
+	// instead of being a related field.
