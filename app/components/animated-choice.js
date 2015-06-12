59122f0690be6ceb03cdf5169f2bd1ac9c4b84c2
Brandon Antonelli <bantonelli07@gmail.com>
2015-06-12 04:21:33 -0400

    - Finish landing page mini-app/game.

--- app/components/animated-choice.js
+++ app/components/animated-choice.js
@@ -2,15 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNames: ['lp-animated-choice', 'lp-choice-genre', 'cds-background-image'],
+	domElement: null,
 	makeVisible: function() {
 		var element = this.get('elementId');
+		var self = this;
 		console.log(element);	    
 		setTimeout(function (){
 			var domElement = document.getElementById(element);
+			self.set('domElement', domElement);
 			domElement.classList.add('lp-animated-choice-visible');
 			// var selector = "#" + element + ".lp-animated-choice";
 			// Ember.$(selector).fadeIn( "slow" );
-		}, 500);		
+		}, 100);		
         // clickElement.classList.remove('icon-pause');
-  	}.on('didInsertElement')
+  	}.on('didInsertElement'),
+  	makeInvisible: function() {	
+		this.get('domElement').classList.remove('lp-animated-choice-visible');
+			// var selector = "#" + element + ".lp-animated-choice";
+			// Ember.$(selector).fadeIn( "slow" );		
+        // clickElement.classList.remove('icon-pause');
+  	}.on('willDestroyElement')
 });
