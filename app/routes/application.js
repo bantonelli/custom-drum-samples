ea150fd982a93e96f5cbd0125244354184ccb39a
Brandon Antonelli <bantonelli07@gmail.com>
2015-06-13 18:27:56 -0400

    - Make account-settings a protected route that routes to log in if session is not authenticated.

--- app/routes/application.js
+++ app/routes/application.js
@@ -31,5 +31,3 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
 		}    	
     }   
 });
-
-// curl -H "Authorization: Bearer c47425efe0e492324167f1844e6e7d3f5b6b62f1" http://127.0.0.1:8000/api/users/1
