7c3d57e7d027a8fb95980f42776c1ab937fc5721
Brandon Antonelli <bantonelli07@gmail.com>
2015-06-15 02:03:01 -0400

    - Finish validations on account-settings route - Tweak algo for sign-up route - Tweak styles for sign up route form 	- reduce margins on inputs and put the margin on the .cds-fieldset to help with style of flag/tooltip. - Tweak styles for the error message flag/tooltip.

--- app/controllers/account-settings.js
+++ app/controllers/account-settings.js
@@ -1,96 +1,115 @@
 import Ember from 'ember';
 import config from '.././config/environment';
+import EmberValidations from 'ember-validations';
 var swal = window.sweetAlert;
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations.Mixin, {
 	needs: ['kitbuilder'],
 	userName: null,
 	email: null,
 	currentPassword: null,
 	newPassword: null,
 	profile: Ember.computed.alias("model"),
+	validations: {
+        userName: {
+            format: {
+                with: /^([a-zA-Z]|\d)+$/, 
+                allowBlank: true, 
+                message: 'must be letters and numbers only',
+                unless: 'resetPasswordRun'
+            }
+        },
+        email: {
+            format: {
+                with: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i, 
+                allowBlank: true, 
+                message: 'must be a valid email address',
+                unless: 'resetPasswordRun'
+            }
+        },
+        currentPassword: {
+            presence: {
+            	message: "Please enter your current account password",
+            	unless: 'updateAccountRun'
+            }
+        },
+        newPassword: {
+        	presence: {
+        		message: "Please enter a new password for your account",
+        		unless: 'updateAccountRun'
+        	}	
+        }
+    },
 	actions: {
+		// currentPassword, newPassword, email, userName
 		resetPassword: function () {
-			// Dont need to send user_id
-			// The view gets user from Auth header.
+			this.set('updateAccountRun', false);
+			this.set('resetPasswordRun', true);
+			// reset Password simply grabs the result of the client side validation
 			var self = this;
-			var currentBlank = false;
-			var newBlank = false;
-			// If the user didn't enter an email value use their current email
-			var current_password;
-			if ((this.get('currentPassword') === null) || (this.get('currentPassword') === "")){				
-				currentBlank = true;
-			} else {
-				current_password = this.get('currentPassword');
-			}
+			self.validate().then(null, function (){
+				if (self.get('isValid')) {
+					// Get current password
+					var current_password;			
+					current_password = self.get('currentPassword');
 
-			// If the user didn't enter a username value use their current username
-			var new_password;
-			if ((this.get('newPassword') === null) || (this.get('newPassword') === "")){
-				newBlank = true;
-			} else {
-				new_password = this.get('newPassword');
-			}
+					// Get new password
+					var new_password;			
+					new_password = self.get('newPassword');
+			
+					var data = {				
+						current_password: current_password,
+						new_password: new_password
+					};
 
-			if (currentBlank && newBlank) {
-				// ********* ALERT ******** //
-				swal({
-				  title: "Error!",
-				  text: "You didn't enter any passwords!",
-				  type: "error",
-				  confirmButtonText: "OK"
-				});				
-			} else {
-				var data = {				
-					current_password: current_password,
-					new_password: new_password
-				};
-
-				Ember.$.ajax({
-					type: "POST",
-					url: config.APP.API_HOST + "/" + config.APP.API_NAMESPACE + '/accounts/password',
-					crossDomain: true,
-					data: data,
-					xhrFields: {
-						withCredentials: true
-					},
-					success: function (data){
-						// ********* ALERT ******** //
+					Ember.$.ajax({
+						type: "POST",
+						url: config.APP.API_HOST + "/" + config.APP.API_NAMESPACE + '/accounts/password',
+						crossDomain: true,
+						data: data,
+						xhrFields: {
+							withCredentials: true
+						},
+						success: function (data){
+							// ********* ALERT ******** //
+			                swal({
+							  title: "Success!",
+							  text: 'Your password has been reset!',
+							  type: "success",
+							  confirmButtonText: "OK"
+							});
+							self.set('newPassword', null);
+							self.set('currentPassword', null);     						
+						}
+					}).fail(function( jqXHR, textStatus ) {
+		                // Error that rises when there is a server error
+		                // Or if there is simply an HTTP error that is raised with the request
+		                var messages = jqXHR.responseJSON;
+		                var message = "";
+		                if (messages.current_password){
+		                	message += messages.current_password.join(', ');
+		                }
+		                // ********* ALERT ******** //
 		                swal({
-						  title: "Success!",
-						  text: 'Your password has been reset!',
-						  type: "success",
+						  title: "Error!",
+						  text: message,
+						  type: "error",
 						  confirmButtonText: "OK"
-						});
-						self.set('newPassword', null);
-						self.set('currentPassword', null);     						
-					}
-				}).fail(function( jqXHR, textStatus ) {
-	                // Error that rises when there is a server error
-	                // Or if there is simply an HTTP error that is raised with the request
-	                var messages = jqXHR.responseJSON;
-	                var message = "";
-	                if (messages.current_password){
-	                	message += messages.current_password.join(', ');
-	                }
-	                // ********* ALERT ******** //
-	                swal({
-					  title: "Error!",
-					  text: message,
-					  type: "error",
-					  confirmButtonText: "OK"
-					});                	
-	        	});
-			}
+						});                	
+		        	});
+	        	} else {
+                    self.set('submissionErrors', self.get('errors'));
+                }
+        	});
 		},
 		updateAccount: function () {
-			var emailBlank = false;
-			var usernameBlank = false;
-			// If the user didn't enter an email value use their current email
+			this.set('resetPasswordRun', false);
+			this.set('updateAccountRun', true);			
 			var temp_email;
 			if ((this.get('email') === null) || (this.get('email') === "")){
+				// If nothing is entered use their existing info and set it so validation can run
 				temp_email = this.get('session.currentUser.email');
-				emailBlank = true;
+				this.set('email', temp_email);
 			} else {
 				temp_email = this.get('email');
 			}
@@ -98,62 +117,74 @@ export default Ember.Controller.extend({
 			// If the user didn't enter a username value use their current username
 			var temp_username;
 			if ((this.get('userName') === null) || (this.get('userName') === "")){
+				// If nothing is entered use their existing info and set it so validation can run				
 				temp_username = this.get('session.currentUser.username');
-				usernameBlank = true;
+				this.set('userName', temp_username);
 			} else {
 				temp_username = this.get('userName');
 			}
 
-			if (emailBlank && usernameBlank) {
-				// ********* ALERT ******** //
+			// Test for any change in username or email. If no change do not make call to server.
+			if (temp_username == this.get('session.currentUser.username') && temp_email == this.get('session.currentUser.email')) {
+				// there is no change to username and email. alert user.
+				// ********* ALERT ******** //				
                 swal({
 				  title: "Error!",
-				  text: "You didn't change your account information!",
+				  text: "You have not made any changes to your account information.",
 				  type: "error",
 				  confirmButtonText: "OK"
-				});   
-			} else {
-				var data = {
-					temp_username: temp_username,
-					temp_email: temp_email
-				}
-
-				Ember.$.ajax({
-					type: "PUT",
-					data: data,
-					url: config.APP.API_HOST + '/api/accounts/update',
-					crossDomain: true,
-					xhrFields: {
-						withCredentials: true
-					},
-					success: function (data) {
-						// ********* ALERT ******** //
-						var message = 'A confirmation email has been sent to: '+ data.temp_email +' \n Follow the instructions to finish updating your account';
-		                swal({
-						  title: "Success!",
-						  text: message,
-						  type: "success",
-						  confirmButtonText: "OK"
-						});   
-					}
-				}).fail(function (jqXHR, textStatus) {
-				    var messages = jqXHR.responseJSON;
-				    var message = "";
-	                if (messages.temp_email){
-	                	message += messages.temp_email.join(', ');
-	                }
-	                if (messages.temp_username){
-	                	message += messages.temp_username.join(', ');
-	                }
-					// ********* ALERT ******** //				
-	                swal({
-					  title: "Error!",
-					  text: message,
-					  type: "error",
-					  confirmButtonText: "OK"
-					});                 		            	 
 				});
-			}
+
+			} else {
+				// old username and/or email have some kind of change so validate and make call to server.
+				var self = this;			
+				self.validate().then(function (){
+	                
+					console.log('got passed validation');
+					var data = {
+						temp_username: temp_username,
+						temp_email: temp_email
+					}
+
+					Ember.$.ajax({
+						type: "PUT",
+						data: data,
+						url: config.APP.API_HOST + '/' + config.APP.API_NAMESPACE + '/accounts/update',
+						crossDomain: true,
+						xhrFields: {
+							withCredentials: true
+						},
+						success: function (data) {
+							// ********* ALERT ******** //
+							var message = 'A confirmation email has been sent to: '+ data.temp_email +' \n Follow the instructions to finish updating your account';
+			                swal({
+							  title: "Success!",
+							  text: message,
+							  type: "success",
+							  confirmButtonText: "OK"
+							});   
+						}
+					}).fail(function (jqXHR, textStatus) {
+					    var messages = jqXHR.responseJSON;
+					    var message = "";
+		                if (messages.temp_email){
+		                	message += messages.temp_email.join(', ');
+		                }
+		                if (messages.temp_username){
+		                	message += messages.temp_username.join(', ');
+		                }
+						// ********* ALERT ******** //				
+		                swal({
+						  title: "Error!",
+						  text: message,
+						  type: "error",
+						  confirmButtonText: "OK"
+						});                 		            	 
+					});	                 	
+				}).catch(function (){
+					self.set('submissionErrors', self.get('errors'));
+				});
+			}			
 		},	
 		loadTemplate: function (template){
 			var kitbuilderController = this.get('controllers.kitbuilder');
--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -32,77 +32,74 @@ export default Ember.Controller.extend(EmberValidations.Mixin, {
 	actions: {
 		register: function () {            
             var controllerSelf = this;
+            controllerSelf.validate().then(function (){                
+                var username = controllerSelf.get('username');
+                var email = controllerSelf.get('email');
+                var password = controllerSelf.get('password');
+                var passwordConfirm = controllerSelf.get('passwordConfirmation');
+               
+                
+                var registerData = {
+                    username: username,
+                    email: email,
+                    password: password
+                };
 
-            controllerSelf.validate().then(null, function (){
-                if (controllerSelf.get('isValid')) {
-                    var username = controllerSelf.get('username');
-                    var email = controllerSelf.get('email');
-                    var password = controllerSelf.get('password');
-                    var passwordConfirm = controllerSelf.get('passwordConfirmation');
-                   
-                    
-                    var registerData = {
-                        username: username,
-                        email: email,
-                        password: password
-                    };
-
-                    function getCookie(name) {
-                        var cookieValue = null;
-                        if (document.cookie && document.cookie !== '') {
-                            var cookies = document.cookie.split(';');
-                            for (var i = 0; i < cookies.length; i++) {
-                                var cookie = Ember.$.trim(cookies[i]);
-                                // Does this cookie string begin with the name we want?
-                                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                                    break;
-                                }
+                function getCookie(name) {
+                    var cookieValue = null;
+                    if (document.cookie && document.cookie !== '') {
+                        var cookies = document.cookie.split(';');
+                        for (var i = 0; i < cookies.length; i++) {
+                            var cookie = Ember.$.trim(cookies[i]);
+                            // Does this cookie string begin with the name we want?
+                            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                                break;
                             }
                         }
-                        return cookieValue;
                     }
+                    return cookieValue;
+                }
 
-                    Ember.$.ajax({
-                        beforeSend: function(xhr) {
-                            var csrftoken = getCookie('mycsrftoken');
-                            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                        },
-                        type: "POST",
-                        url: config.APP.API_HOST + "/api/accounts/register",
-                        crossDomain: true,
-                        data: registerData,
-                        success: function (data) {
-                            console.log(data);
-                            var user = data;
-                            controllerSelf.transitionToRoute('register-thanks', user.id);
-                            // do something with server response data
-                        }                
-                    }).fail(function( jqXHR, textStatus ) {
-                        // Error that rises when there is a server error
-                        // Or if there is simply an HTTP error that is raised with the request
-                        var messages = jqXHR.responseJSON;
-                        var message = "";
-                        if (messages.username){
-                            message += "Username error: " + messages.username.join(', ');
-                        }
-                        if (messages.email){
-                            message += "\n" + "Email error: " + messages.email.join(', ');
-                        }
-                        if (messages.password){
-                            message += "\n" + "Password error: " + messages.password.join(', ');   
-                        }
-                        // ********* ALERT ******** //              
-                        swal({
-                          title: "Error(s)!",
-                          text: message,
-                          type: "error",
-                          confirmButtonText: "OK"
-                        });  
-                    });
-                } else {
-                    controllerSelf.set('submissionErrors', controllerSelf.get('errors'));
-                }                
+                Ember.$.ajax({
+                    beforeSend: function(xhr) {
+                        var csrftoken = getCookie('mycsrftoken');
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    },
+                    type: "POST",
+                    url: config.APP.API_HOST + "/api/accounts/register",
+                    crossDomain: true,
+                    data: registerData,
+                    success: function (data) {
+                        console.log(data);
+                        var user = data;
+                        controllerSelf.transitionToRoute('register-thanks', user.id);
+                        // do something with server response data
+                    }                
+                }).fail(function( jqXHR, textStatus ) {
+                    // Error that rises when there is a server error
+                    // Or if there is simply an HTTP error that is raised with the request
+                    var messages = jqXHR.responseJSON;
+                    var message = "";
+                    if (messages.username){
+                        message += "Username error: " + messages.username.join(', ');
+                    }
+                    if (messages.email){
+                        message += "\n" + "Email error: " + messages.email.join(', ');
+                    }
+                    if (messages.password){
+                        message += "\n" + "Password error: " + messages.password.join(', ');   
+                    }
+                    // ********* ALERT ******** //              
+                    swal({
+                      title: "Error(s)!",
+                      text: message,
+                      type: "error",
+                      confirmButtonText: "OK"
+                    });  
+                });             
+            }).catch(function (){
+                controllerSelf.set('submissionErrors', controllerSelf.get('errors'));
             }); // End validation promise
    
 		}
