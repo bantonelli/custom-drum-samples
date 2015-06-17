b2b5f34a98ee0f4abf1d20a3cd2df58294eadb14
Brandon Antonelli <bantonelli07@gmail.com>
2015-06-16 20:22:48 -0400

    - Got basic setup working 	- Implemented template for component 	- Passed in appropriate variables 	- Calculate proper percentage value to pass into discountBox().

--- app/components/discount-box.js
+++ app/components/discount-box.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ['box'],
     discountBoxFunction: function( currentjQueryObject, percent ) {
         percent = Math.floor( Math.max( 0, Math.min( 100, parseInt( percent || ( percent = 0 ) ) ) ) );
 
@@ -37,7 +38,24 @@ export default Ember.Component.extend({
 		console.log(element);
 		var jqueryElement = Ember.$("#" + element);
 		var numberOfSamples = this.get('numberOfSamples');
-		var minimumCount = this.get('minimumCount'); 	    
-		self.discountBoxFunction(jqueryElement, numberOfSamples)
+		var minimumCount = this.get('minimumCount');
+		var maximumCount = this.get('maximumCount');
+		var countTowardsGoal = numberOfSamples - minimumCount;
+		var goalAmount = maximumCount - minimumCount;
+		var goalReached = false;
+		var goalNotStarted = false;
+		if (numberOfSamples > maximumCount) {
+			goalReached = true;
+		}
+		if (numberOfSamples < minimumCount) {
+			goalNotStarted = true;
+		}		
+		var percentReached = (countTowardsGoal / goalAmount) * 100;
+		if (goalReached){
+			percentReached = 100;
+		} else if (goalNotStarted) {
+			percentReached = 0;
+		}		
+		self.discountBoxFunction(jqueryElement, percentReached)
 	}.on('didInsertElement').observes('numberOfSamples')
 });
