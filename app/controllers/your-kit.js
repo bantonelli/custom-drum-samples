2be3298e8bfb004c7564515845a656cf2bd0f341
Brandon Antonelli <bantonelli07@gmail.com>
2015-06-17 19:10:06 -0400

    - Simple solution to nested each memory leak 	- Implemented series of if blocks in template to dynamically load lists of sample types, since their are only a limited 	amount of types anyway.

--- app/controllers/your-kit.js
+++ app/controllers/your-kit.js
@@ -5,5 +5,57 @@ export default Ember.Controller.extend({
     chosenSampleTypes: function () {
         return this.get('controllers.kitbuilder.samplesChosen').mapBy('_data.type');
     }.property('controllers.kitbuilder.samplesChosen.@each'),
-    uniqueSampleTypes: Ember.computed.uniq('chosenSampleTypes')
+    uniqueSampleTypes: Ember.computed.uniq('chosenSampleTypes'),
+    hasKicks: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Kick') === -1){
+    		return false;
+    	}
+    	return true;
+    }),
+    hasSnares: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Snare') === -1){
+    		return false;
+    	}
+    	return true;
+    }),
+    hasClaps: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Clap') === -1){
+    		return false;
+    	}
+    	return true;
+    }),
+    hasOverheads: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Overhead') === -1){
+    		return false;
+    	}
+    	return true;
+    }),
+    hasPercussion: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Percussion') === -1){
+    		return false;
+    	}
+    	return true;
+    }),
+    hasEffects: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Effect') === -1){
+    		return false;
+    	}
+    	return true;
+    }),    
+    hasLoops: Ember.computed('uniqueSampleTypes', function (){
+    	if (this.get('uniqueSampleTypes').indexOf('Loop') === -1){
+    		return false;
+    	}
+    	return true;
+    })
 });
+
+/*
+        (KICK, 'Kick'),
+        (SNARE, 'Snare'),
+        (CLAP, 'Clap'),
+        (OVERHEAD, 'Overhead'),
+        (PERCUSSION, 'Percussion'),
+        (SOUNDFX, 'Effect'),
+        (LOOP, 'Loop'), 
+*/
